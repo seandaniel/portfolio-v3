082d889be34cd375a2b920a0f4fe64d6bb4b03a3
Sean de Groot <degroot.sd@gmail.com>
2020-09-21 20:00:48 -0400

    Added lazy load

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import './global.scss';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // components
 import Header from './Components/Header/Header.js';
-import About from './Components/About/About.js';
-import Skills from './Components/Skills/Skills.js';
-import Projects from './Components/Projects/Projects.js';
-import Blog from './Components/Blog/Blog.js';
 import Footer from './Components/Footer/Footer.js';
 
-// projects
-import GitCheck from './Components/Projects/GitCheck.js';
-import TheSpecificPress from './Components/Projects/TheSpecificPress.js';
-import Verbeeks from './Components/Projects/Verbeeks.js';
-import QuarantinePursuit from './Components/Projects/QuarantinePursuit.js';
-import FootForward from './Components/Projects/FootForward.js';
-import SeamusGolf from './Components/Projects/SeamusGolf.js';
-import FunPlace from './Components/Projects/FunPlace.js';
+const About = lazy(() => import('./Components/About/About.js'));
+const Skills = lazy(() => import('./Components/Skills/Skills.js'));
+const Projects = lazy(() => import('./Components/Projects/Projects.js'));
+const Blog = lazy(() => import('./Components/Blog/Blog.js'));
 
+const GitCheck = lazy(() => import('./Components/Projects/GitCheck.js'));
+const TheSpecificPress = lazy(() => import('./Components/Projects/TheSpecificPress.js'));
+const Verbeeks = lazy(() => import('./Components/Projects/Verbeeks.js'));
+const QuarantinePursuit = lazy(() => import('./Components/Projects/QuarantinePursuit.js'));
+const FootForward = lazy(() => import('./Components/Projects/FootForward.js'));
+const SeamusGolf = lazy(() => import('./Components/Projects/SeamusGolf'));
+const FunPlace = lazy(() => import('./Components/Projects/FunPlace'));
 
 const App = () => {
   return (
@@ -26,19 +25,23 @@ const App = () => {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="wrapper">
           <Header />
-          <main>
-            <Route exact path="/about" component={About} />
-            <Route exact path="/skills" component={Skills} />
-            <Route exact path="/" component={Projects} />
-            <Route exact path="/git-check" component={GitCheck} />
-            <Route exact path="/the-specific-press" component={TheSpecificPress} />
-            <Route exact path="/verbeeks" component={Verbeeks} />
-            <Route exact path="/quarantine-pursuit" component={QuarantinePursuit} />
-            <Route exact path="/foot-forward" component={FootForward} />
-            <Route exact path="/seamus-golf" component={SeamusGolf} />
-            <Route exact path="/fun-place" component={FunPlace} />
-            <Route exact path="/blog" component={Blog} />
-          </main>
+          <Suspense fallback={<div>Loading</div>}>
+            <Switch>
+              <main>
+                <Route exact path="/about" component={About} />
+                <Route exact path="/skills" component={Skills} />
+                <Route exact path="/" component={Projects} />
+                <Route exact path="/git-check" component={GitCheck} />
+                <Route exact path="/the-specific-press" component={TheSpecificPress} />
+                <Route exact path="/verbeeks" component={Verbeeks} />
+                <Route exact path="/quarantine-pursuit" component={QuarantinePursuit} />
+                <Route exact path="/foot-forward" component={FootForward} />
+                <Route exact path="/seamus-golf" component={SeamusGolf} />
+                <Route exact path="/fun-place" component={FunPlace} />
+                <Route exact path="/blog" component={Blog} />
+              </main>
+            </Switch>
+          </Suspense>
         </div>
         <Footer />
       </BrowserRouter>
