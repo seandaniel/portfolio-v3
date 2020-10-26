a3dd28182386791c6f8b3b27ca72e101d288943e
Sean de Groot <degroot.sd@gmail.com>
2020-10-26 11:24:06 -0400

    About text

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
     <section className="p-img-container">
       <div className="p-container">
         <p>A web developer who has a passion for progressive technology, and well-designed products that provide meaningful solutions. My creativity stems from my passion for snowboarding, while my methodical way of thinking relates to following the Toronto Blue Jays. I am a Juno College graduate who takes pride in writing semantic, reusable code that produces web applications that engage with people, providing the best user experience. </p>
-        <p>My experience consists of building web applications using HTML, CSS compiled with Sass, JavaScript, and jQuery with a strong focus in React. I am confident in using Git to collaborate, working with API integrations, and manipulating information in a database. Meeting web accessibility standards, responsive design, establishing SEO, and being able to manage a project’s scope are skills I possess. Clean interfaces with smooth transitions are adamant in my projects, all through maintainable stylesheets.</p>
+        <p>My experience consists of building web applications using HTML, CSS compiled with Sass, JavaScript, and jQuery with a strong focus on React. I am confident in using Git to collaborate, working with API integrations, and manipulating information in a database. Meeting web accessibility standards, responsive design, establishing SEO, and being able to manage a project’s scope are skills I possess. Clean interfaces with smooth transitions are adamant in my projects, all through maintainable stylesheets.</p>
       </div>
       <div className="img-container">
         <div className="toolbar">
