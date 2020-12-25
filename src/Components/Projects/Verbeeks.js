49a793b55fafb37854fb6d3fbddc6b7793ea22f8
Sean de Groot <degroot.sd@gmail.com>
2020-12-25 12:04:40 -0500

    mockups

--- src/Components/Projects/Verbeeks.js
+++ src/Components/Projects/Verbeeks.js
@@ -12,11 +12,11 @@ const Verbeeks = () => {
       <div className="project-button-container">
         <div className="project-content project-content-3">
           <h2>Verbeek's Farm and Garden Centre</h2>
-          <p>Verbeek's services everyone from the home gardener to the largest farm properties across their residing county. Built and designed through Sass partials with the demographic in mind, conveying the business's information precisely.</p>
-          <h4>HTML / Sass / jQuery</h4>
+          <p>Verbeek's services everyone from the home gardener to the largest farm properties across their residing county. Built and designed through Liquid and Sass with the demographic in mind, showcasing the company's brand and unique products.</p>
+          <h4>Liquid / Sass / Vanilla JavaScript</h4>
           <div className="button-container">
             <a href="https://verbeekslandscaping.com/" target="_blank" rel="noopener noreferrer" className="button">View</a>
-            <a href="https://github.com/seandaniel/Verbeek-s" target="_blank" rel="noopener noreferrer" className="button">Code</a>
+            <a href="https://github.com/seandaniel/verbeeks-v2" target="_blank" rel="noopener noreferrer" className="button">Code</a>
             <Link to="/" className="button button-back button-mobile" aria-label="Back"><FaArrowLeft /></Link>
           </div>
         </div>
@@ -24,10 +24,10 @@ const Verbeeks = () => {
       </div>
       <div className="mock-img-container mock-img-container-3">
         <div className="individual-img-container">
-          <img src={verbeeksDesktop} alt="A MacBook showcasing a brussels block patio" />
+          <img src={verbeeksDesktop} alt="A MacBook showcasing a carousel with products to choose from" />
         </div>
         <div className="individual-img-container">
-          <img src={verbeeksMobile} alt="An iPhone showcasing a card with a flower" />
+          <img src={verbeeksMobile} alt="An iPhone showcasing a fertilizer product" />
         </div>
       </div>
     </section>
