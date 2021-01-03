e0225ab590f3eb30cb5b21aa0c7fb3f0ce6f2201
Sean de Groot <degroot.sd@gmail.com>
2021-01-02 20:03:03 -0500

    About

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
   return (
     <section className="p-img-container">
       <div className="p-container">
-        <p>A web developer who has a passion for progressive technology, and well-designed products that provide meaningful solutions. My creativity stems from my passion for snowboarding, while my methodical way of thinking relates to my time on the golf course and following the Toronto Blue Jays.</p>
+        <p> A web developer who has a passion for well-designed web applications that provide meaningful solutions. My creativity stems from my passion for snowboarding, while my methodical way of thinking relates to my time on the golf course and following the Toronto Blue Jays.</p>
         <p>I build web applications using HTML, Sass, JavaScript, and React, as well as Shopify stores through Liquid. Learning about a new project excites me, as I look forward to aligning with the partner's vision in providing an intuitive digital product. Clean interfaces with smooth transitions are adamant in my projects, including a seamless user experience at the forefront.</p>
       </div>
       <div className="img-container">
